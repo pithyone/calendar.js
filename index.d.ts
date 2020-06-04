0fed6de71bb287cb598cb1dd861e9bd69b737cff
Bing <pithyone@vip.qq.com>
2020-06-04 17:39:53 +0800

    chore: add webpack

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+export function lYearDays(y: number): number;
+
+export function leapMonth(y: number): number;
+
+export function leapDays(y: number): number;
+
+export function monthDays(y: number, m: number): number;
+
+export function solarDays(y: number, m: number): number;
+
+export function toGanZhiYear(lYear: number): string;
+
+export function toAstro(cMonth: number, cDay: number): string;
+
+export function toGanZhi(offset: number): string;
+
+export function getTerm(y: number, n: number): number;
+
+export function toChinaMonth(m: number): string;
+
+export function toChinaDay(d: number): string;
+
+export function getAnimal(y: number): string;
+
+export function solar2lunar(y: number, m: number, d: number): calendar;
+
+export function lunar2solar(y: number, m: number, d: number, isLeapMonth?: boolean): calendar;
+
+export interface calendar {
+    Animal: string;
+    IDayCn: string;
+    IMonthCn: string;
+    Term: string;
+    astro: string;
+    cDay: number;
+    cMonth: number;
+    cYear: number;
+    gzDay: string;
+    gzMonth: string;
+    gzYear: string;
+    isLeap: boolean;
+    isTerm: boolean;
+    isToday: boolean;
+    lDay: number;
+    lMonth: number;
+    lYear: number;
+    nWeek: number;
+    ncWeek: string;
+}
